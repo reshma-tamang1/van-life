9fa36684abe9ac07ef0f5d5c510f5ed0cafee456
Reshma Pakhreen Tamang <pakhrinreshma@gmail.com>
2024-12-04 01:20:08 +1100

    login pages

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
-
     const activeStyles = {
         fontWeight: "bold",
         textDecoration: "underline",
@@ -13,21 +12,29 @@ export default function HostLayout() {
         <>
             <nav className="host-nav">
                 <NavLink
-                    to="/host" end
+                    to="."
+                    end
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Dashboard
                 </NavLink>
 
                 <NavLink
-                    to="/host/income"
+                    to="income"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Income
                 </NavLink>
+                
+                <NavLink
+                    to="vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
 
                 <NavLink
-                    to="/host/reviews"
+                    to="reviews"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
                     Reviews
--- index.jsx
+++ index.jsx
@@ -5,11 +5,19 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
+import Login from "./pages/Login"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
+import AuthRequired from "./components/AuthRequired"
 
 import "./server"
 
@@ -22,12 +30,26 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
           </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
